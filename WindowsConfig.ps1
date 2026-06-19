3aa798597da60b7db7c71bc6a34e997f6f83114f
Will Miller <will@midsummerstudios.com>
2026-06-19 17:09:57 -0400

    Added .ideavimrc config and updated Windows config script.

--- WindowsConfig.ps1
+++ WindowsConfig.ps1
@@ -1,1 +1,2 @@
-New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\dotfiles\common\.config\nvim" 
+New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\dotfiles\common\.config\nvim" -Force
+New-Item -ItemType SymbolicLink -Path "$HOME\.ideavimrc" -Target "$HOME\dotfiles\common\.ideavimrc" -Force
